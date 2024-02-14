d74306f86bc72e7f95e09949a1753c4eac39b00e
Michael Velasco <michaelx.velasco@gmail.com>
2024-02-14 10:48:25 -0700

    Updated gcp icl-cluster module

--- terraform/gcp/modules/icl-cluster/main.tf
+++ terraform/gcp/modules/icl-cluster/main.tf
@@ -1,31 +1,41 @@
 resource "google_container_cluster" "cluster" {
-  name = var.cluster_name
+  name     = var.cluster_name
   min_master_version = var.node_version
   initial_node_count = 1
   remove_default_node_pool = true
   deletion_protection = false
 }
 
-resource "google_container_node_pool" "gpu_pool" {
-  name       = "gpu-pool"
-  cluster    = google_container_cluster.cluster.name
-  node_count = 1
-
-}
-
-resource "google_container_node_pool" "gpu_pool" {
-  name       = "gpu-pool"
+resource "google_container_node_pool" "icl_pool" {
+  name       = "icl-pool"
   cluster    = google_container_cluster.cluster.name
   node_count = 1
 
   node_config {
+    image_type   = "cos_containerd"
     machine_type = var.machine_type
-    image_type = "UBUNTU_CONTAINERD"
+
+    guest_accelerator {
+      type  = var.gpu_model
+      count = 1
+      gpu_driver_installation_config {
+        gpu_driver_version = var.gke_gpu_driver_version
+      }
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+    ]
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
-  deletion_protection = false
-  node_version = var.node_version
-  min_master_version = var.node_version
-  remove_default_node_pool = false
 }
 
 output "network" {
