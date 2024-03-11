0e8972d4be0d3df2359f94a8ae992c751b0c17f1
Pavel Chekin <pbchekin@gmail.com>
2024-03-11 11:00:17 -0700

    Fix CI kubespray workflow (#75)
    * Use icl-ccn-kubespray:0.0.4
    
    * FIXME: add 60m delay after an error to troubleshoot
    
    * Default values for variables in Vagrantfiles
    
    This allows running `vagrant` for troubleshooting without setting env
    vars.
    
    * Revert "FIXME: add 60m delay after an error to troubleshoot"
    
    This reverts commit 2704086cd3f61dfd2a9e095960ac3cc5b25344cc.
    
    * Copy logs if tests failed for kubespray scenario
    
    * FIXME: add 60m delay after an error to troubleshoot
    
    * Parse ssh host, port from output
    
    * Parse ssh host, port from output
    
    * Use search instead of match
    
    * Revert "FIXME: add 60m delay after an error to troubleshoot"
    
    This reverts commit 11fc1d8ebd70b0fe47bbdb3839b9b7d95689c4fb.
    
    * Use bash trap to copy logs and clean up vms

--- scripts/ci/vagrant.sh
+++ scripts/ci/vagrant.sh
@@ -10,8 +10,16 @@ export no_proxy=${X1_NOMAAS_CLUSTER_SUBNET_PREFIX}.0/24,$no_proxy
 echo X1_LIBVIRT_DEFAULT_PREFIX: $X1_LIBVIRT_DEFAULT_PREFIX
 echo X1_NOMAAS_CLUSTER_SUBNET_PREFIX: $X1_NOMAAS_CLUSTER_SUBNET_PREFIX
 
+on_exit() {
+    # To troubleshoot a failed workflow either comment out `vm_cleanup` below or add `sleep 60m`
+    # to keep the virtual machines. For example:
+    # [[ $? -eq 0 ]] || sleep 60m
+    vm_copy_logs
+    vm_cleanup
+}
+
+trap on_exit EXIT
+
 vm_start
 vm_exec ./everything.sh
 vm_exec ./test.sh
-vm_cleanup
-
